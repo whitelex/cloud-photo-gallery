d3e061ae16a6a99da4d74beb1df4f53679ae2b89
Alexei Erchov <Alexei.erchov@gmail.com>
2025-08-21 10:38:53 -0400

    first commit

--- services/photoService.ts
+++ services/photoService.ts
@@ -14,7 +14,17 @@ export const fetchPhotosFromS3 = async (): Promise<Photo[]> => {
     { id: '4', url: 'https://pub-73ed1ce74c3649779fab5923bbcef3c8.r2.dev/20241216_165243%20(1).jpg', alt: '', width: 1000, height: 600 },
     { id: '5', url: 'https://pub-73ed1ce74c3649779fab5923bbcef3c8.r2.dev/20250710_153256.jpg', alt: '', width: 1000, height: 600 },
     { id: '6', url: 'https://pub-73ed1ce74c3649779fab5923bbcef3c8.r2.dev/20250714_171136.jpg', alt: '', width: 1000, height: 600 },
-    { id: '7', url: 'https://pub-73ed1ce74c3649779fab5923bbcef3c8.r2.dev/20250715_111215.jpg', alt: '', width: 1000, height: 600 }
+    { id: '7', url: 'https://pub-73ed1ce74c3649779fab5923bbcef3c8.r2.dev/20250715_111215.jpg', alt: '', width: 1000, height: 600 },
+    { id: '8', url: 'https://pub-73ed1ce74c3649779fab5923bbcef3c8.r2.dev/20250424_163212.jpg', alt: '', width: 1000, height: 600 },
+    { id: '9', url: 'https://pub-73ed1ce74c3649779fab5923bbcef3c8.r2.dev/20250404_122156.jpg', alt: '', width: 1000, height: 600 },
+    { id: '10', url: 'https://pub-73ed1ce74c3649779fab5923bbcef3c8.r2.dev/20250417_140007.jpg', alt: '', width: 1000, height: 600 },
+    { id: '11', url: 'https://pub-73ed1ce74c3649779fab5923bbcef3c8.r2.dev/20250416_180338.jpg', alt: '', width: 1000, height: 600 },
+    { id: '12', url: 'https://pub-73ed1ce74c3649779fab5923bbcef3c8.r2.dev/20250304_192622.jpg', alt: '', width: 1000, height: 600 },
+    { id: '13', url: 'https://pub-73ed1ce74c3649779fab5923bbcef3c8.r2.dev/20231214_183329.jpg', alt: '', width: 1000, height: 600 },
+    { id: '14', url: 'https://pub-73ed1ce74c3649779fab5923bbcef3c8.r2.dev/20231214_183338.jpg', alt: '', width: 1000, height: 600 },
+    { id: '15', url: 'https://pub-73ed1ce74c3649779fab5923bbcef3c8.r2.dev/20241122_135244.jpg', alt: '', width: 1000, height: 600 },
+    { id: '16', url: 'https://pub-73ed1ce74c3649779fab5923bbcef3c8.r2.dev/20240201_173254.jpg', alt: '', width: 1000, height: 600 },
+    { id: '17', url: 'https://pub-73ed1ce74c3649779fab5923bbcef3c8.r2.dev/20250328_152337.jpg', alt: '', width: 1000, height: 600 }
   ];
 
   // The function is async to maintain the same signature as a real API call.
